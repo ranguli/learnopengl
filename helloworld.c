d5ddd609e68a64b7f3d876578250b38065e0f1f0
Joshua Murphy <joshuamurphy71@gmail.com>
2017-10-31 12:15:33 -0230

    Successfully link Lua with CMake and embed a Hello World program!

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,19 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
 
 int main(void) {
+    //A hello world example with embedded Lua
+    int status, result, i;
+    double sum;
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L); /* Load Lua libraries */
+    luaL_dostring(L, "print\"Please print me!\"");
+    lua_close(L);   /* Cya, Lua */
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -22,6 +33,6 @@ int main(void) {
 
     char c;
     scanf("%c", &c);
-
+   
     return 0;
 }
